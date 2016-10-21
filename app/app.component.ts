4f1b94ab66bdbb644b73407dbfe96e28f7610d3f
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-21 08:39:15 -0700

    Edit meal details component input section and edit-meal.component.ts.

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,7 +25,9 @@ export class AppComponent {
   public masterMealList: Meal[] = [
     new Meal("Saag Tofu", "Ate one portion size of Saag Tofu.", 230),
     new Meal("Lobia Curry", "Ate 1/2 portion sixe of Lobia Curry.", 175),
-    new Meal("Buffalo Cauliflower Sandwich", "Felt fancy today, so I ate two of them!", 875)
+    new Meal("Punjabi Chole Bhature", "Remind me to never eat this unhealthy for dinner!", 1345),
+    new Meal("Mexican Bowl", "Rice,beans, and salsa are the only things I need.", 375),
+    new Meal("Falafel Over Rice", "Ate one serving of falafel and a side of saffron rice.", 664)
   ];
   selectedMeal: Meal = null;
   showDetails(clickedMeal: Meal) {
