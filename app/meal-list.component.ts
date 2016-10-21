094604dea6f1444d6e11da07f317a70b22ce69a3
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-21 14:33:00 -0700

    Add some basic styling.

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,11 +4,14 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'meal-list',
   template: `
-    <select (change)="onChangeCalories($event.target.value)" clas="filter">
-      <option value="all" selected="selected">Show All Meal Diaries</option>
-      <option value="low">Show Low Calorie Meals</option>
-      <option value="high">Show High Calorie Meals</option>
-    </select>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8">
+        <select (change)="onChangeCalories($event.target.value)" class="filter">
+          <option value="all" selected="selected">Show All Meal Diaries</option>
+          <option value="low">Show Low Calorie Meals</option>
+          <option value="high">Show High Calorie Meals</option>
+        </select>
   <div *ngFor="let currentMeal of childMealList | calories:selectedCalories">
     <h4>Name: {{ currentMeal.name }}</h4>
     <h4 *ngIf="currentMeal.calories<500" class="low">Calories: {{ currentMeal.calories }}</h4>
